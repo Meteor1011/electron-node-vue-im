a86f227618214607bd51c9de7be7b5e3d826a1f7
huxiaofei <695427170@qq.com>
2018-05-16 11:28:32 +0800

    图片的放大功能

--- app/src/pages/chat/picDialog.vue
+++ app/src/pages/chat/picDialog.vue
@@ -4,7 +4,20 @@
         methods: {
             picDialogClosed(){
                 this.$emit('picDialogClosed',false);
+            },
+            bigimg(i){
+                var zoom = parseInt(i.style.zoom,10)||100;
+                zoom += event.wheelDelta / 12;
+                if(zoom > 0 )
+                i.style.zoom=zoom+'%';
+                return false;
             }
+        },
+        mounted(){
+            var self = this;
+            document.getElementById("aa").addEventListener('mousewheel',function(e){
+                self.bigimg(e.target)
+            })
         }
     };
 </script>
@@ -12,12 +25,10 @@
 <template>
     <div class="pic_dialog" id="pic_dialog">
         <div class="pic_box">
-        <div class="pic_dialog_header">     
             <span class="pic_dialog_closed" v-on:click = "picDialogClosed">x</span>
-        </div>
-        <div class="pic_dialog_body" id="pic_dialog_body">
-            <img :src = "imgsrc" alt="">
-        </div>
+            <div class="pic_dialog_body" id="pic_dialog_body">
+                <img id="aa" :src = "imgsrc" alt="">
+            </div>
         </div>
     </div>
 </template>
@@ -36,13 +47,7 @@
         min-width:220px;
         min-height:220px;
         background: #fff;
-    }
-    .pic_dialog_header{
-        width:100%;
-        height:25px;
-        line-height:25px;
-        position:relative;
-        background:#fff;
+        overflow: auto;
     }
     .pic_dialog_closed{
         position: absolute;
@@ -55,32 +60,30 @@
         text-align: center;
         cursor:pointer;
         line-height:25px;
-        
+        z-index: 30;
     }
     .pic_dialog_closed:hover{
         background:red;
     }
     .pic_box{
-        position:fixed;
+        position:absolute;
         top:50%;
         left:50%;
         text-align:center;
-        max-width:1200px;
+        max-width:922px;
         min-width:220px;
         min-height:220px;
+        max-height: 800px;
         transform:translate(-50%,-50%);
         -ms-transform:translate(-50%,-50%); 	/* IE 9 */
         -moz-transform:translate(-50%,-50%); 	/* Firefox */
         -webkit-transform:translate(-50%,-50%); /* Safari 和 Chrome */
         -o-transform:translate(-50%,-50%); 
+        overflow: hidden;
     }
-    .pic_dialog_body>div{
-        margin-bottom:8px;
-        display:block;
-        text-align:center;
-        vertical-align:middle;
-    }
-    .pic_dialog_body>div>img{
-        max-width:400px;
+    .pic_dialog_body>img{
+        max-width:922px;
+        max-height: 800px;
+        vertical-align: bottom;
     }
 </style>
